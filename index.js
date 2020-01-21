cc062ee7b89235a2b5e9802a35d6a09c2acabb5c
pat-white-code <pat.k.white@gmail.com>
2020-01-21 15:34:07 -0600

    Make a route for a POST to path /locations

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 let port = 8000;
 
 app.use(bodyParser.json());
@@ -21,6 +22,31 @@ app.post('/clients', (req, res)=> {
   }
   clients.push(createdClient)
   res.json(createdClient)
+});
+
+app.post('/locations', (req, res)=> {
+  //req.body example: {"id":3, lat:"30.23",long:"-97.7"}
+  let id = req.body.id;
+  let lat = req.body.lat;
+  let long = req.body.long;
+  let location = {}
+  let options = {
+    method: 'GET',
+    headers: {
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
+      }
+  }
+  fetch(`http://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${long}&zoom=18&addressdetails=1`, options)
+    .then(res => res.json())
+    .then(json => {
+  console.log(json)
+  location.address = json.address.house_number + json.address.road + json.address.city + json.address.state + json.address.postcode;
+  });
+  let client = clients.find(client => client.id === id)
+  client.lat = lat;
+  client.long = long;
+  client.location = location.address;
+  res.send(client);
 })
 
 app.listen(port, ()=> {
